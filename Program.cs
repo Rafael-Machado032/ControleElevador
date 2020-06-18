fc8216a4702e9b8d99e603a2f4cdc8bcc853140b
Rafinha0321 <67079258+Rafinha0321@users.noreply.github.com>
2020-06-17 21:35:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+
+namespace ControleElevador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controle exibir = new Controle();
+            //exibir.andarMenosUtilizado();
+            //exibir.elevadorMaisFrequentado();
+            //exibir.elevadorMenosFrequentado();
+            exibir.percentualDeUsoElevadorE();
+            //exibir.periodoMaiorFluxoElevadorMaisFrequentado();
+            //exibir.periodoMenorFluxoElevadorMenosFrequentado();
+            //exibir.periodoMaiorUtilizacaoConjuntoElevadores();
+        }
+    }
+}
